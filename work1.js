2b695a6b68f4bb09bfb369c725ceeca8647bc004
paipicpla <paipicpla@gmail.com>
2023-10-15 15:31:19 +0900

    branch 생성 후 첫번째 커밋

---
+++ work1.js
@@ -0,0 +1,13 @@
+let actOne = 1;
+function work() {
+    console.log('work1 start-1');
+    console.log('work1 start-2');
+    console.log('work1 start-3');
+    console.log('work1 start-4');
+    console.log('work1 start-5');
+    console.log('work1 start-6');
+    console.log('work1 start-7');
+    console.log('work1 start-8');
+    console.log('work1 start-9');
+    console.log('work1 start-0');
+}
